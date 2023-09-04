cf228be80fc8d7cfca9e8f44df59fa10a5e29209
Jens Andersen <ja@cego.dk>
2023-09-04 13:45:52 +0200

    Add accesskeys for merge requests in list.

--- merge_request_list.js
+++ merge_request_list.js
@@ -1,3 +1,4 @@
+// Add z, x, c and v shortcuts for the different subpages:
 var elOpened = document.getElementById('state-opened');
 elOpened.insertBefore(document.createTextNode('(alt+z) '), elOpened.firstChild);
 elOpened.setAttribute('accesskey', 'z')
@@ -13,3 +14,20 @@ elClosed.setAttribute('accesskey', 'c');
 var elAll = document.getElementById('state-all');
 elAll.insertBefore(document.createTextNode('(alt+v) '), elAll.firstChild);
 elAll.setAttribute('accesskey', 'v');
+
+// Add number shortcuts for the merge requests in the list:
+var mergeRequestContainerList = [];
+document.querySelectorAll('li').forEach((elLi) => {
+    if (elLi.classList.contains('merge-request')) {
+        mergeRequestContainerList.push(elLi);
+    }
+});
+
+var shortcutList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+for (var i = 0; i < Math.min(10, mergeRequestContainerList.length); i++) {
+    var shortcut = shortcutList[i];
+    var branchContainer = mergeRequestContainerList[i];
+    var mergeRequestLink = branchContainer.querySelector('[class="js-prefetch-document"]');
+    mergeRequestLink.insertBefore(document.createTextNode('(alt+' + shortcut + ') '), mergeRequestLink.firstChild);
+    mergeRequestLink.setAttribute('accesskey', shortcut.toString());
+}
